3b469409e5d1bd1445d486331448e003fa7177eb
Gayathri-betha <144835447+Gayathri-betha@users.noreply.github.com>
2024-05-28 09:25:12 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -5,27 +5,27 @@ from langchain.utilities import SQLDatabase
 from langchain.llms import OpenAI
 from langchain.agents.agent_types import AgentType
 from langchain.agents.agent_toolkits import SQLDatabaseToolkit
+from langchain.utilities import SQLDatabase
+from langchain_openai import OpenAI
 from langchain.agents import create_sql_agent
 from dotenv import load_dotenv
 
 st.title("SQL Agent")
 
-# Enter your OpenAI API private access key here. IMPORTANT - don't share your code online if it contains your access key or anyone will be able to access your OpenAI account
-if api_key := st.text_input("Enter the API Key"):
-    os.environ['OPENAI_API_KEY'] = api_key
-    load_dotenv()
-
-# Define the database we want to use for our test
+# Enter your OpenAI API private access key here. IMPORTANT - don't share your code online if it contains your access key or anyone will be able to access your openai account
+api_key = st.secrets["OPENAI_API_KEY"]
+os.environ['OPENAI_API_KEY'] = api_key
+load_dotenv()
+# define the database we want to use for our test
 db = SQLDatabase.from_uri('sqlite:///sql_lite_database.db')
 
-# Choose LLM model, in this case the default OpenAI model
+# choose llm model, in this case the default OpenAI model
 llm = OpenAI(
-    temperature=0,
-    verbose=True,
-    openai_api_key=os.getenv("OPENAI_API_KEY"),
-)
-
-# Setup agent
+            temperature=0,
+            verbose=True,
+            openai_api_key=os.getenv("OPENAI_API_KEY"),
+            )
+# setup agent
 toolkit = SQLDatabaseToolkit(db=db, llm=llm)
 agent_executor = create_sql_agent(
     llm=llm,
@@ -48,6 +48,6 @@ if prompt := st.chat_input("What is up?"):
         st.markdown(prompt)
 
     with st.chat_message("assistant"):
-        response = str(agent_executor.invoke(prompt)["output"])
+        response=str(agent_executor.invoke(prompt)["output"])
         st.write(response)
     st.session_state.messages.append({"role": "assistant", "content": response})
